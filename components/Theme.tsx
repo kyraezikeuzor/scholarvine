d3f0848cf8e2f39a311c8175f13a71567b8942c6
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2024-03-13 00:05:19 -0500

    Temporary theme component changes

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -4,29 +4,26 @@ import React, {useState, useEffect} from 'react'
 import Icon from './Icon'
 
 const ThemeButton = () => {
+
     const [showDarkMode, setShowDarkMode] = useState(false)
     const [systemMode, setSystemMode] = useState('')
+    
 
     useEffect(()=>{
         
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches == true) {
-            setSystemMode('dark')
-        } else {
-            setSystemMode('light')
-        }
+        document.documentElement.classList.toggle('dark', showDarkMode);
 
-        const themeVariable = systemMode === 'light' ? 'dark' : 'light'
-        
-        document.documentElement.classList.toggle(themeVariable, showDarkMode);
     }, [showDarkMode])
 
-    useEffect(()=>{
-        const data = window.localStorage.getItem('SCHOLARVINE_APP_THEME');
-        if (data != null) setShowDarkMode(JSON.parse(data))
-    }, [])
 
     useEffect(()=>{
-        window.localStorage.setItem('SCHOLARVINE_APP_THEME', JSON.stringify(showDarkMode))
+        const data = window.localStorage.getItem('SEDGE_APP_THEME');
+        if (data != null) setShowDarkMode(JSON.parse(data))
+    },[])
+
+
+    useEffect(()=>{
+        window.localStorage.setItem('SEDGE_APP_THEME', JSON.stringify(showDarkMode))
     }, [showDarkMode])
 
     const changeTheme = () => {
